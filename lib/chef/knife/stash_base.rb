4bbba269184c159eec83e572727e2f9a2b6c1b84
Todd Foley <todd.foley@searchoptics.com>
2015-06-04 17:57:45 -0700

    updated options based upon knife plugin docs

--- lib/chef/knife/stash_base.rb
+++ lib/chef/knife/stash_base.rb
@@ -70,6 +70,8 @@ class Chef
         key = key.to_sym
     	rval = config[key] || Chef::Config[:knife][key] || $default[key]
     	Chef::Log.debug("value for config item #{key}: #{rval}")
+    	Chef::Log.debug(Chef::Config[:knife][:node_name])
+    	Chef::Log.debug(Chef::Config[:knife]['node_name'])
     	rval
       end
 
